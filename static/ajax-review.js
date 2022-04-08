ceb8acfc9426b0b650d928204dadad7e82dd7461
karineunderwood <karineunderwood23@gmail.com>
2022-04-07 20:07:15 -0700

    fetch fortune done

---
+++ static/ajax-review.js
@@ -0,0 +1,25 @@
+// PART 1: SHOW A FORTUNE
+
+function showFortune() {
+
+
+  fetch('/fortune')
+    .then(response => response.text())
+    .then(responseFortune => {
+      document.querySelector('#fortune-text').innerHTML = responseFortune;
+    })
+  
+}
+
+document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+  evt.preventDefault();
+  const zipcode = document.querySelector('#zipcode-field').value;
+  const url = `/weather?zipcode=${zipcode}`;
+  // TODO: request weather with that URL and show the forecast in #weather-info
+}
+
+document.querySelector('#weather-form').addEventListener('submit', showWeather);
